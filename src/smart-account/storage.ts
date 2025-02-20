85582970fa8b88f9c770efd5e8159ff3689f57ae
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2025-02-20 10:53:25 +0530

    update storage layer

--- src/smart-account/storage.ts
+++ src/smart-account/storage.ts
@@ -1,34 +1,26 @@
-import {
-  DelegationStorageClient,
-  DelegationStorageEnvironment,
-  DelegationStoreFilter,
-  DelegationStruct,
-} from "@metamask-private/delegator-core-viem";
+import { DelegationStruct } from "@metamask-private/delegator-core-viem";
 import { Address } from "viem";
-
-export const delegationStorageClient = () => {
-  const api = import.meta.env.VITE_API_KEY;
-  const apiKeyId = import.meta.env.VITE_API_KEY_ID;
-  console.log(api, apiKeyId);
-
-  return new DelegationStorageClient({
-    apiKey: api,
-    apiKeyId: apiKeyId,
-    environment: DelegationStorageEnvironment.dev,
-  });
-};
 
 export const getDelegation = async (
   address: Address
 ): Promise<DelegationStruct> => {
-  const delegations = await delegationStorageClient().fetchDelegations(
-    address,
-    DelegationStoreFilter.Received
-  );
+  const delegationJson = localStorage.getItem(address);
+  if (delegationJson) {
+    return JSON.parse(delegationJson);
+  }
 
-  return delegations[0];
+  throw new Error("Delegation not found");
 };
 
 export const storeDelegation = async (delegation: DelegationStruct) => {
-  await delegationStorageClient().storeDelegation(delegation);
+  const json = JSON.stringify(delegation, (_, value) => {
+    if (typeof value === "bigint") {
+      return value.toString();
+    }
+    return value;
+  });
+
+  console.log("Delegation Json", json);
+
+  localStorage.setItem(delegation.delegate, json);
 };
